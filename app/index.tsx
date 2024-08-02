4c530f39fab6d49e78629bc093e707ae5bb3fff7
Sajit Dahal <SajitDahal77@kbc.edu.np>
2024-08-02 22:33:44 +0545

    Added SplashScreen Image

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,8 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 import beachImage from '@/assets/meditation-images/beach.webp';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import CustomBtn from '@/components/CustomBtn';
 
 const App = () => {
   return (
@@ -17,11 +19,17 @@ const App = () => {
         className='flex-1'
         colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}
         >
-          <SafeAreaView className='flex-1 px-1 justify-between'>
+          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
         <View>
           <Text className='text-center text-white font-bold text-4xl '>Simple Meditation</Text>
-          <Text className='text-center text-white text-regular text-2xl mt-3'>Simplifying Meditation For Everyone</Text>
+          <Text className='text-center text-white text-regular text-1.5xl mt-3'>Simplifying Meditation For Everyone</Text>
         </View>
+        <View>
+          <CustomBtn onPress={()=> console.log('tap')}
+          title='Get Started' />
+        </View>
+        
+        <StatusBar style='light' />
           </SafeAreaView>
         </LinearGradient>
         
